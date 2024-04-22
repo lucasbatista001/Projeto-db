727a7d6cf525334d0dcf5a5d45549fc1c1a0fac2
Vinicius Rocha <viniciusroch65@gmail.com>
2024-04-22 16:54:30 -0300

    Update database configuration and add default values for user, password, and database in db.ts

--- src/db.ts
+++ src/db.ts
@@ -2,9 +2,9 @@ import { Client } from "pg";
 const client = new Client({
   host: process.env.DB_HOST ?? "localhost",
   port: Number(process.env.DB_PORT) ?? 5432,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  user: process.env.DB_USER ?? "postgres",
+  password: process.env.DB_PASSWORD ?? "123456",
+  database: process.env.DB_NAME ?? "postgres",
 });
 
 await client.connect();
