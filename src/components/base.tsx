f537b08a0bb9f12d1ee8470f018911e50100fb01
lucasbatista001 <lucasgalindobatista@gmail.com>
2024-05-19 19:47:42 -0300

    style/base

--- src/components/base.tsx
+++ src/components/base.tsx
@@ -19,7 +19,7 @@ export const Base = ({ children }: { children: JSX.Element }) => (
         crossorigin="anonymous"
       ></script>
     </head>
-    <body class={"h-screen"}>
+    <body class={"h-screen bg-gray-100"}>
       <nav class="bg-white border-gray-200 dark:bg-gray-900">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a
@@ -72,34 +72,11 @@ export const Base = ({ children }: { children: JSX.Element }) => (
               </li>
               <li>
                 <a
+                  onclick=''
                   href="#"
                   class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
-                  
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  
+                  Equipe
                 </a>
               </li>
             </ul>
--- src/components/details.tsx
+++ src/components/details.tsx
@@ -14,14 +14,17 @@ export const Details = ({
       class={"w-full flex items-center justify-between"}
     >
       <textarea name="content" class={"block p-2.5 w-full max-h-80 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"} />
-      <button
+      
+      <div class={'px-5'}>
+        <button
         type="submit"
         class={
           "flex p-2 w-fit bg-green-500 text-center rounded hover:opacity-[0.8]"
         }
-      >
-        Comentar
-      </button>
+        >
+          Comentar
+        </button>
+      </div>
     </form>
   </div>
 );
